29dc57e203a95c22025e7997da5dd2483a47b121
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2021-03-18 09:50:08 +0530

    March - 18 Code
    March - 18 Code

--- Sample.java
+++ Sample.java
@@ -1,41 +1,102 @@
 package com.corejava.sample;
 
-public class Sample {
-
-	static int bonus = 1500;	
-	int personSalary = 4545454;
+public class Sample{
 	
-	public static void main(String[] args) {		
-		// This is a single line comment		
-		/*
-		 This is for commenting block of  code
-		 All these lines  will be commented...		  
-		 */
+	static int bonus = 5000 ;	
+	private int personSalary = 4545454;
+	static boolean isMarried;
+	
+	public static void main(String[] args) {	
+		
+		System.out.println("inside the main method....starting");
+		
+		// Declaration
+		int age;		
+		// Initialiation
+		age = 20;
+		
+		// both in one line
+		final int comm = 0;
+		
+		//comm = 500;		
+		boolean result = false;
+		boolean isMarried  = true;
+		
+		//System.out.println("Commison--" +comm);
+		//System.out.println("Bonus--"+bonus);		
+		System.out.println("isMarried----"+isMarried);
+		System.out.println("result---"+result);
+		
+		method1();
+		System.out.println("bonus---"+bonus);	
+		
+		
+		System.out.println("isMarried----"+isMarried);
+		
+		System.out.println("Ending the main method....");
+		
+	/*
+		
 		
 		System.out.println("Welcome to java by patil....");
 		System.out.println("Java seems to be interesting.....");		
-		int age = 20;
+		
 		System.out.println(age);		
 		System.out.println("The employee name is Ramu and his age is "+age);
 		method1();		
-		System.out.println(bonus);
+		
+		
+		Sample s = new Sample();		
+		System.out.println(s.personSalary);
+		s.method2();		
+		
 		System.out.println("Ending the main method....");
+		
+		if (bonus == 2000)
+		{
+			System.out.println("The emp is eligible for a promotion");
+		}
+		else
+		{
+			System.out.println("The emp is not eligible for a promotion");
+		}
+		*/	
+		
 	}
-	
+
 	public static void method1()
 	{
+		System.out.println("inside method1  ");
 		int sal = 5000;
-		System.out.println("inside method1 , sal is "+sal);
+		int comm = 1500;
+		
+		int income = (sal + comm);
+		System.out.println(income);
+		System.out.println("Ending method1  ");
+		
+		
+		/*
 		System.out.println(bonus);
+		//method2();
+		bonus = 10000;
+		System.out.println(bonus);
+		//Sample s = new Sample();
+		//s.method2();
+		*/
+		
 	}
-	
+
 	public void method2()
 	{
 		System.out.println("inside method 2 this is non static method....");
 		System.out.println(personSalary);
+		
+		
 		System.out.println(bonus);
+		//method1();
+		
 	}
-	
-	
+
+
 
 }
