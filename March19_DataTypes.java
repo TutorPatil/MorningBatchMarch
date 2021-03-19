444e91894077fe610e65ccbc5e6e0c8d81925799
TutorPatil <30532209+TutorPatil@users.noreply.github.com>
2021-03-19 09:48:27 +0530

    March-19 Code
    March-19 Code

---
+++ March19_DataTypes.java
@@ -0,0 +1,80 @@
+package com.corejava.sample;
+
+public class March19_DataTypes {
+	
+	static int age; 
+	static boolean isMarried ;
+
+	public static void main(String[] args) {
+		
+		byte b = 100;
+		
+		//byte b1 = 130;
+		
+		System.out.println(age);
+		System.out.println(isMarried);
+		
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		
+		short s ;		
+		
+		int i = 454463;
+		
+		long l = 43445;
+		
+		long l1 = 4544646464646466L;
+		
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Integer.MAX_VALUE);
+		
+		float f = 10.54F;
+		
+		double d = 45.545;
+		
+		char c = 'a';
+		
+		char c1 = 'x';
+		
+		char c2 = '#';
+		
+		char c3 = 65;
+		
+		System.out.println(c3);
+		
+		boolean isMarried = true;
+		
+		boolean isSeniorCitizen  = false;
+		
+		
+		System.out.println("My name is patil \n I am your trainer");
+		
+		System.out.println("My name is patil\tI am your trainer");
+		
+		String filePath = "c:\\users\\naveen\\temp";
+		
+		String str = "I am a proud \"Indian\" , I love \'my\' country";
+		
+		System.out.println(str);
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
